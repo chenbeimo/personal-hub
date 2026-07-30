ee07cc36d302680256ef3b244a35063931a20bd0
Personal Hub <1546429045@qq.com>
2026-07-30 21:27:23 +0800

    Remove PWA plugin completely

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons/*.svg'],
-      manifest: {
-        name: '个人工作台 - Personal Hub',
-        short_name: '个人工作台',
-        description: '每日计划、习惯追踪、灵感记录、学习管理',
-        theme_color: '#a855f7',
-        background_color: '#f3e8ff',
-        display: 'standalone',
-        scope: '/personal-hub/',
-        start_url: '/personal-hub/',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'icons/icon-192x192.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          },
-          {
-            src: 'icons/icon-512x512.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-        skipWaiting: true,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   base: '/personal-hub/',
 })
